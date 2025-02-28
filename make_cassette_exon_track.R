ac865ec4c6e4257e867c2bea0168b269c3ebb990
yuliya-ant <79752490+yuliya-ant@users.noreply.github.com>
2025-02-28 21:38:25 +0300

    Update make_cassette_exon_track.R

--- make_cassette_exon_track.R
+++ make_cassette_exon_track.R
@@ -8,21 +8,26 @@ library(tibble)
 
 setwd("~/Data/Lab2/")
 
-path1 = 'srsf3_kd_cassete_exons.tsv'
-path2 = 'rbfox2_kd_cassete_exons.tsv'
-path_out <- 'srsf3_rbfox2_kd_cassette_exons.bed'
+# path1 = 'srsf3_kd_cassete_exons.tsv'
+# path2 = 'rbfox2_kd_cassete_exons.tsv'
+# path_out <- 'srsf3_rbfox2_kd_cassette_exons.bed'
 
 
 path1 = 'chx_cassette_exons_mouse.csv'
 path_out = 'chx_cassette_exons_mouse.bed'
 
-path1 = 'chx_cassette_exons_human.csv'
-path_out = 'chx_cassette_exons_human.bed'
-  
+# path1 = 'chx_cassette_exons_human.csv'
+# path_out = 'chx_cassette_exons_human.bed'
+
+
+# UCSC track columns:
 # chrom chromStart chromEnd name score strand 
 # thickStart thickEnd itemRgb blockCount blockSizes blockStarts
+#
+# https://genome.ucsc.edu/goldenPath/help/customTrack.html - here find 'BED custom track with multiple blocks'
+# 
 
-# psi = (i1 + i2) / (2e + i1 + i2)
+# psi = 2e / (2e + i1 + i2)
 
   
 make_ce_bed = function(ce, colorx) {
@@ -63,15 +68,15 @@ ce_bed2 = make_ce_bed(ce, color2)
 
 View(ce_bed1)
 
-header <- 'track name="chx_cassette" description="chx_cassette_exons, PSI = (i1 + i2) / (2e + i1 + i2)" visibility=2 color=103,137,33 itemRgb="on" useScore=1'
+header <- 'track name="chx_cassette" description="chx_cassette_exons, PSI = 2e / (2e + i1 + i2)" visibility=2 color=103,137,33 itemRgb="on" useScore=1'
 
-header <- 'track name="srsf3_kd_cassette" description="srsf3_kd_cassette_exons, PSI = (i1 + i2) / (2e + i1 + i2)" visibility=2 color=103,137,33 itemRgb="on" useScore=1'
+header <- 'track name="srsf3_kd_cassette" description="srsf3_kd_cassette_exons, PSI = 2e / (2e + i1 + i2)" visibility=2 color=103,137,33 itemRgb="on" useScore=1'
 cat(header, '\n', file=path_out, append = FALSE)
 write.table(ce_bed1,
             file = path_out, append = TRUE, sep=' ', 
             quote = FALSE, row.names = FALSE, col.names = FALSE)
 
-header <- 'track name="rbfox2_kd_cassette" description="rbfox2_kd_cassette_exons, PSI = (i1 + i2) / (2e + i1 + i2)" visibility=2 color=71,48,120 itemRgb="on" useScore=1'
+header <- 'track name="rbfox2_kd_cassette" description="rbfox2_kd_cassette_exons, PSI = 2e / (2e + i1 + i2)" visibility=2 color=71,48,120 itemRgb="on" useScore=1'
 cat(header, '\n', file=path_out, append = TRUE)
 write.table(ce_bed2,
             file = path_out, append = TRUE, sep=' ', 
